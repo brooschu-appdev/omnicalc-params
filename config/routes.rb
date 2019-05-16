e3f6a8ab512afb61a1246748ce87dad2633714e2
Brooke Schuler <brooke.schuler@kellogg.northwestern.edu>
2019-05-16 05:25:10 +0000

    square forms and results RCAV both working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
     match("/flexible/square/:zebra", { :controller => "calc", :action => "flex_square", :via => "get" })
-end
+    match("/flexible/payment/:zebra/:giraffe/:elephant", { :controller => "calc", :action => "flex_pmt", :via => "get" })
+    match("/square/new", { :controller => "calc", :action => "square_form", :via => "get" })
+    match("/square/results", { :controller => "calc", :action => "process_square_form", :via => "get" })
+end 
